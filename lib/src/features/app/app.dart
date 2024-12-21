f6dd1da6b13289789fec775721838f5d00a76c39
nur93batken <usonisaev67@gmail.com>
2024-12-21 15:54:28 +0600

    Бул жерде өзгөртүүлөрдүн кыскача сүрөттөмөсү

--- lib/src/features/app/app.dart
+++ lib/src/features/app/app.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
-import '../home/presentation/pages/home.dart';
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHome(),
-    );
-  }
-}
+export 'pages/app_view.dart';
